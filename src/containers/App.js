379475db59014bc81968cb8c8a5d63c9732e3664
Tsg170001 <tsg170001@utdallas.edu>
2023-09-22 14:15:55 -0500

    Re-structured files for clarity and created production build for deployment

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import Cardlist from './CardList';
+import Cardlist from '../components/CardList';
 import 'tachyons';
-import SearchBox from "./SearchBox";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
     constructor(){
@@ -23,19 +24,21 @@ class App extends Component {
         this.setState({ searchfield: event.target.value })
     }
     render() {
-        
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        if(this.state.robots.length === 0){
+        if(robots.length === 0){
             return <h1 className='tc'> Loading</h1>
         } else {
             return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <Cardlist robots={filteredRobots}/>
+                <Scroll>
+                    <Cardlist robots={filteredRobots}/>
+                </Scroll>
             </div>
         );}
     }
