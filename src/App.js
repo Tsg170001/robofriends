81f63462a835c6de248b9d5a5f766cabba51773e
Tsg170001 <tsg170001@utdallas.edu>
2023-09-22 13:31:10 -0500

    Changed data formatting from being in .js file into a json api call using fetch()

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Cardlist from './CardList';
 import 'tachyons';
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount(){
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -19,18 +24,20 @@ class App extends Component {
     }
     render() {
         
-        const filteredRobots = this.state.robots.filter(robot => {
+        const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        console.log(filteredRobots);
 
-        return(
+        if(this.state.robots.length === 0){
+            return <h1 className='tc'> Loading</h1>
+        } else {
+            return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Cardlist robots={filteredRobots}/>
             </div>
-        );
+        );}
     }
 }
 
